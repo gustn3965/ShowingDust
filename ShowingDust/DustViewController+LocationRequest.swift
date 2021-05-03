e8b02fbd19c5d93d1fc77e08815d9f68a7716213
gustn3965 <gustn3965@gmail.com>
2021-05-03 20:42:47 +0900

    implementing get user location

---
+++ ShowingDust/DustViewController+LocationRequest.swift
@@ -0,0 +1,44 @@
+//
+//  DustViewController+LocationRequest.swift
+//  ShowingDust
+//
+//  Created by hyunsu on 2021/05/03.
+//
+
+
+import CoreLocation
+
+extension DustViewController: CLLocationManagerDelegate {
+
+    func getUserLocation()  {
+        if checkAuthorization() {
+            print(locationManager.location)
+        }
+    }
+
+    func checkAuthorization() -> Bool {
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways:
+            return true
+        case .authorizedWhenInUse:
+            return true
+        default:
+            locationManager.requestWhenInUseAuthorization()
+        }
+        return false
+    }
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch locationManager.authorizationStatus {
+        case .authorizedAlways:
+            getUserLocation()
+        case .authorizedWhenInUse:
+            locationManager.requestAlwaysAuthorization()
+            getUserLocation()
+        default:
+            return
+        }
+    }
+
+    
+}
--- ShowingDust/DustViewController.swift
+++ ShowingDust/DustViewController.swift
@@ -6,24 +6,19 @@
 //
 
 import UIKit
-
+import CoreLocation
 class DustViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // TODO: - 유저위치 권한받아오기
+    @IBAction func touchpUpCurrent() {
+        getUserLocation()
     }
-    */
-
 }
+
+
